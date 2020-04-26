36577ada5daed2d803834cfeea6163af216c68cc
Yaroslav Erohin <ysoftware@yandex.ru>
2020-04-26 14:34:26 +0300

    add user code project

---
+++ UserCode/SwiftMetaUserCode/SwiftMetaUserCode/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftMetaUserCode
+//
+//  Created by Ерохин Ярослав Игоревич on 26.04.2020.
+//  Copyright © 2020 Yaroslav Erokhin. All rights reserved.
+//
+
+import Foundation
+
+
